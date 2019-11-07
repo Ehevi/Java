9e949e315bd725a259c7d4e07b3903454599bc0a
Ehevi <48785655+Ehevi@users.noreply.github.com>
2019-11-07 15:15:33 +0100

    Update Animal.java

--- lab4/src/agh/cs/lab4/Animal.java
+++ lab4/src/agh/cs/lab4/Animal.java
@@ -3,17 +3,31 @@ package agh.cs.lab4;
 import agh.cs.lab2.MapDirection;
 import agh.cs.lab2.MoveDirection;
 import agh.cs.lab2.Vector2d;
+import agh.cs.lab4.IWorldMap;
 
 public class Animal
 {
     private MapDirection orientation = MapDirection.NORTH; //okresla poczatkowa orientacje zwierzecia jako NORTH
     private Vector2d position = new Vector2d(2,2); //poczatkowe polozenie zwierzecia na mapie
     //zdefiniowanie pol jako PRYWATNE!
+    private IWorldMap;
+
+    public Animal()
+    {
+        super();
+    }
 
     public Animal(IWorldMap map)
+    //konstruktor
     {
-        this.orientation=MapDirection.NORTH;
-        this.position=new Vector2d(2, 2);
+        this.map = map;
+    }
+
+    public Animal(IWorldMap map, Vector2d initialPosition)
+    //konstruktor, ktory dodatkowo okresla poczatkowe polozenie zwierzecia na mapie
+    {
+        this.position = initialPosition;
+        this.map = map;
     }
 
     public void move(MoveDirection direction)
@@ -36,10 +50,14 @@ public class Animal
                 break;
         }
 
-        if(temporaryPosition.x >=0 && temporaryPosition.x <=4 && temporaryPosition.y >= 0 && temporaryPosition.y <= 4)
+        if((map != null && map.canMoveTo(temporaryPosition))) 
+        //odwolanie do mapy i sprawdzenie, czy zwierze moze sie przesunac na dana pozycje
         {
             this.position = temporaryPosition;
-        } //sprawdzenie, czy nie wyjachalam zwierzeciem poza mape i ewentualny powrot do pozycji poczatkowej
+        }
+//      else if(map == null &&  (temporaryPosition.x >=0 && temporaryPosition.x <= 4 && temporaryPosition.y >= 0 && temporaryPosition.y <= 4))
+//      {this.position = temporaryPosition;}
+        //sprawdzenie, czy nie wyjachalam zwierzeciem poza mape i ewentualny powrot do pozycji poczatkowej
     }
 
     public MapDirection getOrientation()
@@ -48,18 +66,16 @@ public class Animal
     }
 
     public Vector2d getPosition()
+        //zwraca pozycje zwierzecia
     {
         return this.position;
     }
 
     @Override
-    public String toString() //informacje o polozeniu zwierzecia
+    public String toString()
+    //zwraca schematyczną orientację zwierzęcia
     {
-        StringBuilder str = new StringBuilder();
-        str.append("Orientation: ");
-        str.append(this.orientation.name()); //orientacja zwierzecia
-        str.append("\nPosition: ");
-        str.append(this.position); //pozycja zwierzecia
-        return str.toString();
+        String s = this.orientation.name();
+        return s.charAt(0)+"";
     }
 }
